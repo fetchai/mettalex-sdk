21a3cc8daa30baec6e77a1fed16bf4d4bedb427e
Ankit Mahajan <ankit.mahajan@fetch.ai>
2021-01-27 19:30:30 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="Mettalex",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A package for SDK of Mettalex dex",
+    long_description="An sdk package to perform trades on Mettalex dex",
+    long_description_content_type="text/markdown",
+    url="https://github.com/fetchai/mettalex-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['pathlib', 'argparse','requests', 'web3'],
+    python_requires='>=3.0',
+)
